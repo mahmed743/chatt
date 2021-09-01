15757f4ab6a9b3d6932424c2cfba6174ba9bb093
Mustafa Elbashier <m.ahmed743@gmail.com>
2021-09-01 19:30:18 +0200

    frist

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDB4mysiPy1VLYxUSXI40JZDNj-BeIT-yQ",
+  authDomain: "chatangular-cce4d.firebaseapp.com",
+  databaseURL: "https://chatangular-cce4d-default-rtdb.firebaseio.com/",
+  projectId: "chatangular-cce4d",
+  storageBucket: "chatangular-cce4d.appspot.com",
+  messagingSenderId: "219729352800",
+  appId: "1:219729352800:web:5fe8fa35433a2f2ed1cb05"
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-chat';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import * as firebase from 'firebase';
+import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export const snapshotToArray = (snapshot: any) => {
+  const returnArr = [];
+
+  snapshot.forEach((childSnapshot: any) => {
+    const item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+  return returnArr;
+};
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  nickname = '';
+  ref = firebase.database().ref('users/');
+  matcher = new MyErrorStateMatcher();
+  isLoadingResults = true;
+  users: any;
+  constructor(private router: Router,
+    private snackBar: MatSnackBar,
+    public datepipe: DatePipe
+    , private formBuilder: FormBuilder, public httpClient: HttpClient) { }
+
+  ngOnInit() {
+    // if (localStorage.getItem('nickname')) {
+    //   this.router.navigate(['/roomlist']);
+    // }
+    this.loginForm = this.formBuilder.group({
+      'nickname': [null, Validators.required]
+    });
+    this.httpClient.get<any>('https://www.delmon-egate.com/api/MainUsers').subscribe(data => {
+      console.log(data.value);
+      this.users = data.value;
+      this.users.forEach(element => {
+        this.createUserRooms(element.UserName);
+      });
+      this.isLoadingResults = false;
+    })
+  }
+  onSubmit(value) {
+    this.nickname = value;
+    //  console.log(value);
+    this.ref.orderByChild('nickname').equalTo(value).once('value', snapshot => {
+      if (snapshot.exists()) {
+        localStorage.setItem('nickname', value);
+        this.createUserRoom(value);
+      } else {
+        const newUser = firebase.database().ref('users/').push();
+        newUser.set(value);
+        localStorage.setItem('nickname', value);
+        this.createUserRoom(value);
+      }
+    });
+  }
+  createUserRoom(user) {
+    const room = { roomname: user };
+    this.ref.orderByChild('roomname').equalTo(room.roomname).once('value', (snapshot: any) => {
+      if (snapshot.exists()) {
+        this.enterChatRoom(user);
+      } else {
+        const newRoom = firebase.database().ref('rooms/').push();
+        newRoom.set(room);
+        this.enterChatRoom(user);
+      }
+    });
+  }
+
+  createUserRooms(user) {
+    const room = { roomname: user };
+    this.ref.orderByChild('roomname').equalTo(room.roomname).once('value', (snapshot: any) => {
+      if (snapshot.exists()) {
+        this.snackBar.open('Room name already exist!');
+      } else {
+        const newRoom = firebase.database().ref('rooms/').push();
+        newRoom.set(room);
+      }
+    });
+  }
+
+
+  enterChatRoom(user) {
+    const chat = { roomname: '', nickname: '', message: '', date: '', type: '' };
+    chat.roomname = user;
+    chat.nickname = this.nickname;
+    chat.date = this.datepipe.transform(new Date(), 'dd/MM/yyyy HH:mm:ss');
+    chat.message = `${this.nickname} enter the room`;
+    chat.type = 'join';
+    const newMessage = firebase.database().ref('chats/').push();
+    newMessage.set(chat);
+
+    firebase.database().ref('roomusers/').orderByChild('roomname').equalTo(user).on('value', (resp: any) => {
+      let roomuser = [];
+      roomuser = snapshotToArray(resp);
+      const user = roomuser.find(x => x.nickname === this.nickname);
+      if (user !== undefined) {
+        const userRef = firebase.database().ref('roomusers/' + user.key);
+        userRef.update({ status: 'online' });
+      } else {
+        const newroomuser = { roomname: '', nickname: '', status: '' };
+        newroomuser.roomname = user;
+        newroomuser.nickname = this.nickname;
+        newroomuser.status = 'online';
+        const newRoomUser = firebase.database().ref('roomusers/').push();
+        newRoomUser.set(newroomuser);
+      }
+    });
+
+    this.router.navigate(['/chatroom', this.nickname]);
+  }
+
+}
